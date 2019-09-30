dc4fe2447fe502627f6a093011c7ffefb03701f3
nikithakethireddy1996 <54380887+nikithakethireddy1996@users.noreply.github.com>
2019-09-30 13:14:19 -0500

    edited code

--- main.js
+++ main.js
@@ -13,12 +13,12 @@ const validate = async (event) => {
 
 const updateWithMultiply = async (event) => {
   document.querySelector('#result').innerHTML = ''
-  if (document.querySelector('#Length').checkValidity() && document.querySelector('#Breadth').checkValidity() && document.querySelector('#Height').checkValidity()) {
+  if (document.querySelector('#studentsPerCourse').checkValidity() && document.querySelector('#coursesPerTerm').checkValidity() && document.querySelector('#countOfTerms').checkValidity()) {
     const regex = /[^a-zA-Z_]/g
     const s = document.querySelector('#guest').value.replace(regex, '')
-    const i = parseInt(document.querySelector('#Length').value)
-    const j = parseInt(document.querySelector('#Breadth').value)
-    const k = parseInt(document.querySelector('#Height').value)
+    const i = parseInt(document.querySelector('#studentsPerCourse').value)
+    const j = parseInt(document.querySelector('#coursesPerTerm').value)
+    const k = parseInt(document.querySelector('#countOfTerms').value)
     const ans = `${s}, your product is ${multiply(i, j, k)}.`
     document.querySelector('#result').innerHTML = ans
   }
@@ -28,9 +28,9 @@ const updateWithMultiply = async (event) => {
 // focusout is like blur, but it bubbles up
 
 document.addEventListener('focusout', event => {
-  if ((event.target && event.target.id === 'Length') ||
-    (event.target && event.target.id === 'Breadth')
-    (event.target && event.target.id === 'Height')) {
+  if ((event.target && event.target.id === 'studentsPerCourse') ||
+    (event.target && event.target.id === 'BcoursesPerTerm')
+    (event.target && event.target.id === 'countOfTerms')) {
     validate(event)
   }
 })
